427df148b42c3fe9c6b4b2c3ca9ce9d1107fe1cd
Quantzz <nichlas.rasmussen@gmail.com>
2018-07-07 22:56:45 +0200

    fixed a few bugs

--- supremeBot/main.py
+++ supremeBot/main.py
@@ -20,6 +20,7 @@ class supremeBot(object):
                 self.base_url,
                 self.shop,
                 self.info['category'])).text
+        soup = bs.BeautifulSoup(r, 'lxml')
 
         temp_tuple = []
         temp_link = []
@@ -28,39 +29,39 @@ class supremeBot(object):
             temp_tuple.append((link['href'], link.text))
 
         for i in temp_tuple:
-            if i[1].lower() == self.info['product'].lower() or i[1] == self.info['color'].lower():
+            if i[1] == self.info['product'] or i[1] == self.info['color']:
                 temp_link.append(i[0])
 
         self.final_link = list(
             set([x for x in temp_link if temp_link.count(x) == 2]))[0]
 
     def visitSite(self):
-        b.visit(
-            "{}{}{}".format(
-                self.base_url, self.shop, str(
+        self.b.visit(
+            "{}{}".format(
+                self.base_url, str(
                     self.final_link)))
-        browser.find_option_by_text(self.info['size']).click()
-        browser.find_by_value('add to basket').click()
+        self.b.find_option_by_text(self.info['size']).click()
+        self.b.find_by_value('add to basket').click()
 
         time.sleep(0.5)
-        browser.visit("{}{}".format(self.base_url, self.checkout))
+        self.b.visit("{}{}".format(self.base_url, self.checkout))
 
-        browser.fill("order[billing_name]", self.info['namefield'])
-        browser.fill("order[email]", self.info['emailfield'])
-        browser.fill("order[tel]", self.info['phonefield'])
+        self.b.fill("order[billing_name]", self.info['namefield'])
+        self.b.fill("order[email]", self.info['emailfield'])
+        self.b.fill("order[tel]", self.info['phonefield'])
 
-        browser.fill("order[billing_address]", self.info['addressfield'])
-        browser.fill("order[billing_city]", self.info['city'])
-        browser.fill("order[billing_zip]", self.info['zipfield'])
-        browser.select("order[billing_country]", self.info['country'])
+        self.b.fill("order[billing_address]", self.info['addressfield'])
+        self.b.fill("order[billing_city]", self.info['city'])
+        self.b.fill("order[billing_zip]", self.info['zipfield'])
+        self.b.select("order[billing_country]", self.info['country'])
 
-        browser.select("credit_card[type]", self.info['card'])
-        browser.fill("credit_card[cnb]", self.info['number'])
-        browser.select("credit_card[month]", self.info['month'])
-        browser.select("credit_card[year]", self.info['year'])
-        browser.fill("credit_card[vval]", self.info['ccv'])
-        browser.find_by_css('.terms').click()
-        browser.find_by_value("process payment").click()
+        self.b.select("credit_card[type]", self.info['card'])
+        self.b.fill("credit_card[cnb]", self.info['number'])
+        self.b.select("credit_card[month]", self.info['month'])
+        self.b.select("credit_card[year]", self.info['year'])
+        self.b.fill("credit_card[vval]", self.info['ccv'])
+        self.b.find_by_css('.terms').click()
+        self.b.find_by_value("process payment").click()
 
     def main(self):
         self.initializeBrowser()
